26ccd04220ba1643c1f29338a73697248d9dc47a
Regent'sVoice <139999765+RegentsVoice@users.noreply.github.com>
2024-12-17 17:40:20 +0300

    Delete Mikrotik VPN  Back to home WEB panel directory

--- Mikrotik VPN  Back to home WEB panel/const.py
+++
@@ -1,4 +0,0 @@
-RouteIP = "192.168.88.1"
-RouteUsername = "YOUR_USERNAME"
-RoutePassword = "YOUR_PASSWORD"
-RoutePort = "22"
--- Mikrotik VPN  Back to home WEB panel/panelsetting.py
+++
@@ -1,3 +0,0 @@
-GServerIp = "127.0.0.1"
-GServerPort = 7862
-GPassword = "P@ssw0rd"
